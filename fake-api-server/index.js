b8e0052088461f77e86e99849f0cf465a5d33e40
adm-sabitovka <sabitovka@shkd.bizml.ru>
2022-07-21 11:04:46 +0300

    Added starting json server

--- fake-api-server/index.js
+++ fake-api-server/index.js
@@ -1,3 +1,7 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 const { compileObjects, generateObjects } = require('./compile.js');
 
 const args = process.argv.slice(2);
@@ -5,6 +9,13 @@ const args = process.argv.slice(2);
 if (args.includes('--generate')) {
   generateObjects();
 }
+
 if (args.includes('--compile')) {
   compileObjects();
 }
+
+server.use(middlewares)
+server.use(router)
+server.listen(3001, () => {
+  console.log('Fake API Server is running on port 3001...')
+})
